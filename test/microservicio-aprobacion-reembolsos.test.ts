f1b6d1c1bbe5e1f76e1c8cf50d61e9f87ead1c52
grestrepo98 <gustavo.restrepo@nuatechnology.com>
2025-05-05 15:52:29 -0500

    Initial commit

---
+++ test/microservicio-aprobacion-reembolsos.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MicroservicioAprobacionReembolsos from '../lib/microservicio-aprobacion-reembolsos-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/microservicio-aprobacion-reembolsos-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MicroservicioAprobacionReembolsos.MicroservicioAprobacionReembolsosStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
