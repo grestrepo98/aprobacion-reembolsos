b9e0fd95d53c3ec325ad214f1e7cbcbc23345021
grestrepo98 <gustavo.restrepo@nuatechnology.com>
2025-05-07 20:37:21 -0500

    agrega registros de consola para depuración en LambdaA y LambdaB

--- lib/resources/step-functions/lambda/LambdaA/index.ts
+++ lib/resources/step-functions/lambda/LambdaA/index.ts
@@ -1,5 +1,6 @@
 export const handler = async (event: any) => {
   // Entrada esperada: { name: "Juan" }
+  console.log('event', event);
   const saludo = `Hola, ${event.name}`;
   return { saludo };
 };
--- lib/resources/step-functions/lambda/LambdaB/index.ts
+++ lib/resources/step-functions/lambda/LambdaB/index.ts
@@ -1,5 +1,6 @@
 export const handler = async (event: any) => {
   // Entrada: { saludo: "Hola, Juan" }
+  console.log('event', event);
   const mensajeFinal = `${event.saludo}. ¡Bienvenido al sistema!`;
   return { mensajeFinal };
 };
