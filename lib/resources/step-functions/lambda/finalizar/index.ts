6945113f41e4dfbc418f8648b43aaba83ebfba04
grestrepo98 <gustavo.restrepo@nuatechnology.com>
2025-05-08 08:59:03 -0500

    feature: Simplifica la estructura del evento en la función Lambda de finalización, eliminando la interfaz y ajustando el acceso a los resultados

--- lib/resources/step-functions/lambda/finalizar/index.ts
+++ lib/resources/step-functions/lambda/finalizar/index.ts
@@ -1,24 +1,13 @@
 import { Handler } from 'aws-lambda';
 
-// interface Event {
-//   ValidarEmail: { emailValido: boolean };
-//   ValidarSaldo: { saldoSuficiente: boolean };
-// }
-
-// type Event = [{ emailValido: boolean }, { saldoSuficiente: boolean }];
-
-interface Event {
-  emailValido: boolean;
-  saldoSuficiente: boolean;
-}
-
+type Event = [{ emailValido: boolean }, { saldoSuficiente: boolean }];
 interface Response {
   validacionCompleta: string;
 }
 
 export const handler: Handler<Event, Response> = async (event) => {
   console.log('event', event);
-  const todoOk = event.emailValido && event.saldoSuficiente;
+  const todoOk = event[0].emailValido && event[1].saldoSuficiente;
   return {
     validacionCompleta: todoOk ? 'TODO_OK' : 'FALLO_VALIDACION',
   };
